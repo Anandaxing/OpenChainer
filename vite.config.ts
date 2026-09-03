8363aecca181aae0a3f140ead2a2e538429bdd56
Ananda12 <ananda.xing12@gmail.com>
2026-09-03 18:23:53 +0800

    chore(nitro): install package nitro for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nitro } from "nitro/types";
 
 const config = defineConfig({
 	resolve: { tsconfigPaths: true },
@@ -15,6 +16,7 @@ const config = defineConfig({
 		}),
 		tailwindcss(),
 		tanstackStart(),
+		nitro(),
 		viteReact(),
 		babel({ presets: [reactCompilerPreset()] }),
 	],
